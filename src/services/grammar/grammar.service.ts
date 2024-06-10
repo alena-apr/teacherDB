3e46ce18d11ddcdbfa7d97a3644d72d1d5d8c5c5
alena-apr <a1-a1-a1@yandex.ru>
2024-06-10 18:43:46 +0300

    done smth

--- src/services/grammar/grammar.service.ts
+++ src/services/grammar/grammar.service.ts
@@ -5,26 +5,4 @@ import { IGrammar } from 'src/interfaces/grammar';
 import { Grammar, GrammarDocument } from 'src/schemas/grammar';
 
 @Injectable()
-export class GrammarService {
-
-        constructor(
-        @InjectModel(Grammar.name) private grammarModel: Model<GrammarDocument>,
-    ) {
-        console.log('GrammarService run');
-    }
-
-    async getAllGrammar(): Promise<IGrammar[]> {
-        return this.grammarModel.find();
-    }
-
-    async getGrammarById(id:string): Promise<IGrammar> {
-        return this.grammarModel.findById(id);
-    }
-
-    async sendGrammar(data): Promise<IGrammar> {
-        const ExData = new this.grammarModel({...data});
-        return ExData.save();
-
-    }
-}
-
+export class GrammarService {}
